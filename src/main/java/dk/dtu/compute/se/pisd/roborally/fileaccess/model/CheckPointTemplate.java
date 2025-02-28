b2815ae66398ec8e2db47a9aa29937ca1d2181d2
BenTheNavigator <benjaminlgad@gmail.com>
2025-02-28 12:00:20 +0100

    checkpointemplate comment added

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/model/CheckPointTemplate.java
@@ -0,0 +1,14 @@
+package dk.dtu.compute.se.pisd.roborally.fileaccess.model;
+
+
+/**
+ * Template class for checkpoint feature implementation.
+ * @s235444
+ */
+public class CheckPointTemplate extends ActionTemplate{
+
+    public int number;
+
+    public boolean last;
+
+}
